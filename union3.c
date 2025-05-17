ac4f51e8ef38d01c02399eb69467fa3e3416071d
aditya0tomar <adityasinghtomar0432@gmail.com>
2025-05-17 14:23:39 +0530

    17-05-25

--- union3.c
+++ union3.c
@@ -14,3 +14,22 @@ ptr = &var;
 printf("The value of a is : %d", ptr->a);
 return 0;
 }
+
+//user defind
+
+// #include <stdio.h>
+// union high{
+//     int a;
+//     int b;
+// };
+// int main()
+// {
+//     union high * ptr;
+//     union high var ;
+//     printf("enter the value:\n");
+//     scanf("%d",&var.a);
+//     var.a ;
+//     ptr = &var;
+//     printf("The Value of a is :%d",ptr ->a);
+//     return 0;
+// }
