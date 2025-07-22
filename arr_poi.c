948f2c22281bd12ac8d67beed6c2e7d0e25725db
aditya0tomar <adityasinghtomar0432@gmail.com>
2025-07-22 21:35:39 +0530

    Initial commit

--- arr_poi.c
+++ arr_poi.c
@@ -7,3 +7,5 @@ int main(){
        printf("%c",*(name+7));
 return 0;
 }
+// This program demonstrates how to access characters in a string using pointer arithmetic.
+// It initializes a string and prints specific characters by dereferencing pointers to the string's elements.
